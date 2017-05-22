da8a121e6eb59a4edd08f434b63a34cebf69ce21
piotrpawlaczek <info@pawlaczek.pl>
2017-05-22 22:06:23 +0200

    added basic CI

--- wanna/settings.py
+++ wanna/settings.py
@@ -17,8 +17,8 @@ class Config(object):
         config.read(os.path.expanduser('~/.wanna/credentials'))
 
         self.PARTNER_NAME = config.get('default', 'partner', fallback=self.PARTNER_NAME)
-        self.ENCRYPTION_KEY = bytes(bytearray.fromhex(config.get('default', 'encryption_key')))
-        self.UPLOAD_PREFIX = config.get('default', 'upload_prefix')
+        self.ENCRYPTION_KEY = bytes(bytearray.fromhex(config.get('default', 'encryption_key', fallback='0000')))
+        self.UPLOAD_PREFIX = config.get('default', 'upload_prefix', fallback='not-set')
         self.BUCKET = config.get('default', 'bucket', fallback=self.BUCKET)
         self.VENDOR = DATACENTERS[vendor.name.lower()](config)
 
@@ -26,8 +26,8 @@ class Config(object):
 class AWS(object):
 
     def __init__(self, cfg):
-        self.API_KEY = cfg.get('aws', 'aws_access_key_id')
-        self.API_SECRET = cfg.get('aws', 'aws_secret_access_key')
+        self.API_KEY = cfg.get('aws', 'aws_access_key_id', fallback='missing')
+        self.API_SECRET = cfg.get('aws', 'aws_secret_access_key', fallback='missing')
 
 
 DATACENTERS = {
