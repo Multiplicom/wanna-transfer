242937980e173110a82bf065a96647b7d7247c86
Michiel Vermeir <michiel.vermeir@agilent.com>
2019-01-04 16:14:41 +0100

    default overrides & profiles #5

--- wanna/entry_points/wannacli.py
+++ wanna/entry_points/wannacli.py
@@ -28,7 +28,7 @@ Options:
   --no-encrypt   Do not encrypt at rest.
   --no-decrypt   Do not decrypt in transit.
   --ignore-prefix  Ignore all prefixes
-  --profile=<name>  Use a named profile [default: aws]
+  --profile=<name>  Use a named profile
   --datacenter=<name>  Cloud provider [default: aws]
   --bucket=<name>  Bucket name [default: credentials]
 """
