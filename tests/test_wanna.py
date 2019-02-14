d03f1d6e3f61e5fc5b4ff581931838cdbb9f1cf1
Michiel Vermeir <michiel.vermeir@agilent.com>
2019-02-14 14:37:37 +0100

    tests have been superseded by tests_config

--- tests/test_wanna.py
+++ tests/test_wanna.py
@@ -13,28 +13,3 @@ def test_transfer():
     trf = Transfer()
     assert hasattr(trf, "download_file")
 
-
-def test_default_profile():
-    with patch.object(
-        configparser.ConfigParser, "get", return_value="0000"
-    ) as cfg_get_mocked:
-        trf = Transfer()
-
-        cfg_get_mocked.assert_any_call("aws", "aws_access_key_id", fallback="missing")
-        cfg_get_mocked.assert_any_call(
-            "aws", "aws_secret_access_key", fallback="missing"
-        )
-
-
-def test_named_profile():
-    with patch.object(
-        configparser.ConfigParser, "get", return_value="0000"
-    ) as cfg_get_mocked:
-        trf = Transfer(profile="partner")
-
-        cfg_get_mocked.assert_any_call(
-            "partner", "aws_access_key_id", fallback="missing"
-        )
-        cfg_get_mocked.assert_any_call(
-            "partner", "aws_secret_access_key", fallback="missing"
-        )
