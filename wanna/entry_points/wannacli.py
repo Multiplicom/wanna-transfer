4a0b35e9b2c54395b53221f3b796343d9341173b
Michiel <6299345+michielvermeir@users.noreply.github.com>
2019-09-09 11:20:25 +0200

    fix: reset color codes accordingly

--- wanna/entry_points/wannacli.py
+++ wanna/entry_points/wannacli.py
@@ -131,21 +131,24 @@ def handle_download(args):
             # Possible causes:
             # * The object was encrypted using a different method
             # * The object was not encrypted but the operation tried to decrypt it
-            print("{}Bad Request: Object encryption doesn't match parameters.".format(codes['darkred']))
+            print("{}Bad Request: Object encryption doesn't match parameters.{}".format(
+              codes['darkred'],
+              codes['reset']
+            ))
             print("Try downloading using --no-decrypt")
 
         elif 'Forbidden' in error.args[0]:
             # Possible causes:
             # * The file was encrypted using a different encryption key
             # * The bucket or object ACL disallows this operation for the user accessing the object
-            print("{}Forbidden: You don't have the correct permissions to access the requested object.".format(
+            print("{}Forbidden: You don't have the correct permissions to access the requested object.{}".format(
                 codes['darkred'],
                 codes['reset']
             ))
 
         elif 'Not Found' in error.args[0]:
             LOG.error("Error: {}".format(error))
-            print("{}**Error** File not found.".format(
+            print("{}**Error** File not found.{}".format(
                 codes['darkred'],
                 codes['reset']
             ))
